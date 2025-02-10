0bbbe1753f259a97f0cf1a23435f9e1bead3f42c
Yanay Zabary <yanay.tsabary@gmail.com>
2025-02-10 14:00:59 +0200

    update

--- components/sheets/CommentSectionSheet.tsx
+++ components/sheets/CommentSectionSheet.tsx
@@ -30,136 +30,74 @@ import { showMessage } from "react-native-flash-message";
 import useSheetManager from "../../hooks/useSheetManager";
 import BottomSheetWrapper from "./BottomSheetWrapper";
 
-const CommentsComponents = memo(
-  ({
-    entityId,
-    navigateToAccount,
-    navigateToProfile,
-  }: {
-    entityId: string | null | undefined;
-    navigateToAccount: (accountId: string) => void;
-    navigateToProfile: () => void;
-  }) => {
-    const { highlightedCommentId, closeCommentSectionDrawer } =
-      useSheetManager();
-    const customStyles = useMemo<Partial<UseSocialStyleProps>>(
-      () => ({
-        newCommentFormProps: {
-          verticalPadding: 16,
-          paddingLeft: 24,
-          paddingRight: 24,
-        },
-      }),
-      []
-    );
-    const styleConfig = useSocialStyle(customStyles);
-
-    const callbacks: SocialStyleCallbacks = useMemo(
-      () => ({
-        currentUserClickCallback: () => {
-          Keyboard.dismiss();
-          closeCommentSectionDrawer?.();
-          navigateToProfile();
-        },
-        otherUserClickCallback: (userId) => {
-          Keyboard.dismiss();
-          closeCommentSectionDrawer?.();
-          navigateToAccount(userId);
-        },
-        loginRequiredCallback: () => {
-          showMessage({
-            message: "Oops! Login Required",
-            description: "Please sign in or create an account to continue.",
-            type: "warning",
-          });
-        },
-      }),
-      []
-    );
-
-    const {
-      CommentSectionProvider,
-      CommentsFeed,
-      NewCommentForm,
-      SortByButton,
-    } = useSocialComments({
-      entityId,
-      styleConfig,
-      highlightedCommentId,
-      callbacks,
-    });
-
-    const commentFormRef = useRef<{ focus: () => void } | null>(null);
-
-    useEffect(() => {
-      if (!entityId) return;
-      const timeout = setTimeout(() => {
-        if (commentFormRef.current) {
-          commentFormRef.current.focus();
-        }
-      }, 1000);
-      return () => clearTimeout(timeout);
-    }, [entityId]);
-
-    return (
-      <CommentSectionProvider>
-        <BottomSheetView className="relative flex-1 h-full">
-          <View className="flex-row gap-2 px-4 items-center mb-2">
-            <View className="flex-1" />
-            <SortByButton
-              priority="top"
-              activeView={
-                <Text className="bg-black py-2 px-3 rounded-md text-white text-sm">
-                  Top
-                </Text>
-              }
-              nonActiveView={
-                <Text className="bg-gray-200 py-2 px-3 rounded-md text-sm">
-                  Top
-                </Text>
-              }
-            />
-            <SortByButton
-              priority="new"
-              activeView={
-                <Text className="bg-black py-2 px-3 rounded-md text-white text-sm">
-                  New
-                </Text>
-              }
-              nonActiveView={
-                <Text className="bg-gray-200 py-2 px-3 rounded-md text-sm">
-                  New
-                </Text>
-              }
-            />
-          </View>
-
-          <CommentsFeed />
-          {/* <View className="border-t-hairline border-gray-300"> */}
-          <NewCommentForm ref={commentFormRef} />
-          {/* </View> */}
-        </BottomSheetView>
-      </CommentSectionProvider>
-    );
-  },
-  (prevProps, nextProps) => {
-    return prevProps.entityId === nextProps.entityId;
-  }
-);
-
 const CommentSectionSheet = () => {
+  const router = useRouter();
   const {
     commentSetionSheetRef,
     commmentsEntityId,
     setCommentsEntityId,
     setHighlightedCommentsId,
+    highlightedCommentId,
+    closeCommentSectionDrawer,
   } = useSheetManager();
 
   const snapPoints = useMemo(() => ["100%"], []);
 
   const [sheetOpen, setSheetOpen] = useState(false);
+  const customStyles = useMemo<Partial<UseSocialStyleProps>>(
+    () => ({
+      newCommentFormProps: {
+        verticalPadding: 16,
+        paddingLeft: 24,
+        paddingRight: 24,
+      },
+    }),
+    []
+  );
+  const styleConfig = useSocialStyle(customStyles);
 
-  const router = useRouter();
+  const callbacks: SocialStyleCallbacks = useMemo(
+    () => ({
+      currentUserClickCallback: () => {
+        Keyboard.dismiss();
+        closeCommentSectionDrawer?.();
+        router.navigate("/(tabs)/profile");
+      },
+      otherUserClickCallback: (userId) => {
+        Keyboard.dismiss();
+        closeCommentSectionDrawer?.();
+        router.navigate(`/account/${userId}`);
+      },
+      loginRequiredCallback: () => {
+        showMessage({
+          message: "Oops! Login Required",
+          description: "Please sign in or create an account to continue.",
+          type: "warning",
+        });
+      },
+    }),
+    []
+  );
+
+  const { CommentSectionProvider, CommentsFeed, NewCommentForm, SortByButton } =
+    useSocialComments({
+      entityId: commmentsEntityId,
+      styleConfig,
+      highlightedCommentId,
+      callbacks,
+    });
+
+  const commentFormRef = useRef<{ focus: () => void } | null>(null);
+
+  useEffect(() => {
+    if (!commmentsEntityId) return;
+    const timeout = setTimeout(() => {
+      if (commentFormRef.current) {
+        commentFormRef.current.focus();
+      }
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [commmentsEntityId]);
 
   const renderBackdrop = useCallback(
     (props: BottomSheetBackdropProps) => (
@@ -192,13 +130,42 @@ const CommentSectionSheet = () => {
           behavior={Platform.OS === "ios" ? "padding" : "height"}
           className="flex-1"
         >
-          <CommentsComponents
-            entityId={commmentsEntityId}
-            navigateToAccount={(accountId: string) =>
-              router.navigate(`/account/${accountId}`)
-            }
-            navigateToProfile={() => router.navigate("/(tabs)/profile")}
-          />
+          <CommentSectionProvider>
+            <BottomSheetView className="relative flex-1 h-full">
+              <View className="flex-row gap-2 px-4 items-center mb-2">
+                <View className="flex-1" />
+                <SortByButton
+                  priority="top"
+                  activeView={
+                    <Text className="bg-black py-2 px-3 rounded-md text-white text-sm">
+                      Top
+                    </Text>
+                  }
+                  nonActiveView={
+                    <Text className="bg-gray-200 py-2 px-3 rounded-md text-sm">
+                      Top
+                    </Text>
+                  }
+                />
+                <SortByButton
+                  priority="new"
+                  activeView={
+                    <Text className="bg-black py-2 px-3 rounded-md text-white text-sm">
+                      New
+                    </Text>
+                  }
+                  nonActiveView={
+                    <Text className="bg-gray-200 py-2 px-3 rounded-md text-sm">
+                      New
+                    </Text>
+                  }
+                />
+              </View>
+
+              <CommentsFeed />
+              <NewCommentForm ref={commentFormRef} />
+            </BottomSheetView>
+          </CommentSectionProvider>
         </KeyboardAvoidingView>
       </BottomSheet>
     </BottomSheetWrapper>
